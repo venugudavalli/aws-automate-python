e2e1c4ad4f3612de17951e48311b1524bda5ffe5
Venu Gudavalli <venugudavalli@hotmail.com>
2019-10-21 11:36:47 +0100

    Code refactored for pep8, per pycodestyle, pydocstyle and pylint utilities

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -1,44 +1,68 @@
-import boto3
-import click
-from botocore.exceptions import ClientError
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# pylint: disable=C0321
+
+"""Webotorn: Deploy websites with aws SDK Python.
+
+Automates the process of deploying static websites.
+- Configure AWS s3 buckets
+    - Create AWS s3 BUCKET
+    - Set them up for static websites
+    - Deploy local files to them
+- Configure DNS with AWS Route53
+- Configure a content delivery Network and SSL with AWS CloudFront
+"""
+
 from pathlib import Path
 import mimetypes
+
+import boto3
+from botocore.exceptions import ClientError
+import click
+
+
 session = boto3.Session(profile_name='default')
 s3 = session.resource('s3')
 
+
 @click.group()
 def cli():
-    "Webotron deploys websites to AWS"
-    pass
+    """Webotron deploys websites to AWS."""
+
+
 @cli.command('list-buckets')
 def list_buckets():
-    "List all s3 buckets"
+    """List all s3 buckets."""
     for bucket in s3.buckets.all():
         print(bucket)
+
+
 @cli.command('list-bucket-objects')
 @click.argument('bucket')
 def list_bucket_objects(bucket):
-    "List objects in an s3 bucket"
+    """List objects in an s3 bucket."""
     for obj in s3.Bucket(bucket).objects.all():
         print(obj)
+
 
 @cli.command('setup-bucket')
 @click.argument('bucket')
 def setup_bucket(bucket):
-
-    "Create and configure S3 bucket, enable website "
+    """Create and configure S3 bucket, enable website."""
     s3_bucket = None
     try:
         s3_bucket = s3.create_bucket(
             Bucket=bucket,
-            CreateBucketConfiguration={'LocationConstraint': session.region_name}
-            )
-    except ClientError as e:
-        #print(e.response)
-        if e.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
+            CreateBucketConfiguration={
+                'LocationConstraint': session.region_name
+                }
+                )
+    except ClientError as error:
+        # print(e.response)
+        if error.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
             s3_bucket = s3.Bucket(bucket)
         else:
-            raise e
+            raise error
 
     policy = """{
         "Version":"2012-10-17",
@@ -56,39 +80,45 @@ def setup_bucket(bucket):
     pol = s3_bucket.Policy()
     pol.put(Policy=policy)
 
-    ws = s3_bucket.Website()
-    ws.put(
+    s3_bucket.Website().put(
         WebsiteConfiguration={
-            'ErrorDocument':{
+            'ErrorDocument': {
                 'Key': 'error.html'
                 },
             'IndexDocument': {
-                'Suffix':'index.html'
+                'Suffix': 'index.html'
             }
         })
-    return
+
+
 def upload_file(s3_bucket, path, key):
+    """Upload an object to s3 Bucket."""
     content_type = mimetypes.guess_type(key)[0] or 'text/plain'
     s3_bucket.upload_file(
-    path,
-    key,
-    ExtraArgs={
-        'ContentType': 'text/html'
+        path,
+        key,
+        ExtraArgs={
+            'ContentType': content_type
+            })
 
-    })
+
 @cli.command('sync')
 @click.argument('pathname', type=click.Path(exists=True))
 @click.argument('bucket')
 def sync(pathname, bucket):
-    "Sync contents of PATHNAME to BUCKET"
+    """Sync contents of PATHNAME to BUCKET."""
     s3_bucket = s3.Bucket(bucket)
     root = Path(pathname).expanduser().resolve()
+
     def handle_directory(target):
-        for p in target.iterdir():
-            if p.is_dir(): handle_directory(p)
-            if p.is_file(): upload_file(s3_bucket,str(p),str(p.relative_to(root)))
-            #print(F"Path: {p}\n key: {p.relative_to(root)}")
+        for path in target.iterdir():
+            if path.is_dir():
+                handle_directory(path)
+            if path.is_file():
+                upload_file(s3_bucket, str(path), str(path.relative_to(root)))
+            # print(F"Path: {p}\n key: {p.relative_to(root)}")
     handle_directory(root)
+
 
 if __name__ == "__main__":
     cli()
