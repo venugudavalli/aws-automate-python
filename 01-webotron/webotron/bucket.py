6cf6a8e290f3848e4a956dda398b301916c53eca
Venu Gudavalli <venugudavalli@hotmail.com>
2019-10-22 13:30:48 +0100

    Bucket.py style errors fixed

--- 01-webotron/webotron/bucket.py
+++ 01-webotron/webotron/bucket.py
@@ -5,21 +5,24 @@
 from pathlib import Path
 import mimetypes
 from hashlib import md5
+import boto3
 from botocore.exceptions import ClientError
-import util
+from functools import reduce
+from webotron import util
 
 
 class BucketManager:
     """Manage an s3 bucket."""
+
     CHUNK_SIZE = 8388608
 
     def __init__(self, session):
         """Create a Bucket Manager object."""
         self.session = session
         self.s3 = session.resource('s3')
-        self.transfer_config = boto.s3.transfer.TransferConfig(
-            multipart_chunksize = CHUNK_SIZE,
-            multipart_threshold = CHUNK_SIZE
+        self.transfer_config = boto3.s3.transfer.TransferConfig(
+            multipart_chunksize=self.CHUNK_SIZE,
+            multipart_threshold=self.CHUNK_SIZE
             )
         self.manifest = {}
 
@@ -93,39 +96,58 @@ class BucketManager:
 
     def load_maifest(self, bucket):
         """Load manifest for cahcing purposes."""
+        # print ("Loading manifest")
         paginator = self.s3.meta.client.get_paginator('list_objects_v2')
-        for page in paginator.paginate(Bucket=bucket.name'):
-            for obj in page.get('Contents',[]):
+        for page in paginator.paginate(Bucket=bucket.name):
+            for obj in page.get('Contents', []):
                 self.manifest[obj['Key']] = obj['ETag']
 
     @staticmethod
     def hash_data(data):
         """Generate md5 hash for data."""
         hash = md5()
-        hash.update(path)
+        hash.update(data)
         return hash
 
-    def gen_etag(self, filename):
+    def gen_etag(self, path):
         """Get ETag for file."""
-        hash = None
-        with open(file, 'rb') as f:
-            data = f.read()
-            hash = self.hash_data(data)
+        hashes = []
+        with open(path, 'rb') as f:
+            while True:
+                data = f.read(self.CHUNK_SIZE)
+                if not data:
+                    break
+            hashes.append(self.hash_data(data))
 
-            return F'"{hash.hexdigest()}"'
+        if not hashes:
+            return
+        elif len(hashes) == 1:
+            return F'"{hashes[0].hexdigest()}"'
+        else:
+            hash = self.hash_data(
+                        reduce(
+                            lambda x, y: x + y,
+                            (h.digest() for h in hashes)
+                            )
+                        )
+            return F'"{hash.hexdigest()}-{len(hashes)}"'
 
-    @staticmethod
-    def upload_file(bucket, path, key):
+
+    def upload_file(self, bucket, path, key):
         """Upload an object to s3 Bucket."""
         content_type = mimetypes.guess_type(key)[0] or 'text/plain'
+        etag = self.gen_etag(path)
+        if self.manifest.get(key, '') == etag:
+            # print(F"Skipping {key}, etags match")
+            return
         return bucket.upload_file(
             path,
             key,
             ExtraArgs={
                 'ContentType': content_type
-                },
+                    },
                 Config=self.transfer_config
-                )
+            )
 
     def sync(self, pathname, bucket_name):
         """Sync local website folder with s3 bucket."""
